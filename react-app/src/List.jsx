3d0641c29c86cc2b667fac70c6a3fe1e60ded1b4
Belal <derbelalrahimi@gmail.com>
2025-07-07 08:08:59 +0200

    more render lists and conditional rendering

--- react-app/src/List.jsx
+++ react-app/src/List.jsx
@@ -11,10 +11,18 @@ function List(){
 
     const carsItems = cars.map(car => <li key={car.id}>{car.name}:&nbsp;
                                {car.model}</li>);
+    
+    const newModels = cars.filter(car => car.model > 2012);
+
+    const newModelItems = newModels.map(newModel => <li 
+                                        key={newModel.id}>{newModel.name}:&nbsp;
+                                        {newModel.model}</li>);
 
     return(
         <>
-        <ul>{carsItems}</ul>
+        {cars.length > 0 ? <ul>{carsItems}</ul> : null}
+        <h2>New Models</h2>
+        {cars.length > 0 ? <ol>{newModelItems}</ol> : null}
         </>
     );
 }
